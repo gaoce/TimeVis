0183ae30c8a024d5eba540c23fc9880f9725554c
gaoce <gaoce@coe.neu.edu>
2015-04-26 20:40:32 -0400

    Add templates

--- timevis/app.py
+++ timevis/app.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
 from flask import Flask
 
 app = Flask('timevis')
+
+
+def main():
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
+if __name__ == '__main__':
+    main()
--- timevis/views.py
+++ timevis/views.py
@@ -1,7 +1,13 @@
 from . import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+
+@app.route('/OK')
+def OK():
+    return "OK"
