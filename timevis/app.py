0e7cc2cf4c9fb5b742b85eb0bd82261d10105e18
gaoce <gaoce@coe.neu.edu>
2015-04-26 21:42:43 -0400

    Add webbrowser

--- timevis/app.py
+++ timevis/app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 from flask import Flask
+import webbrowser
 
 # app = Flask('timevis')
 app = Flask('timevis')
 
 
-def main():
+def main(port=8000):
+    # Begin the server
     app.run(host='0.0.0.0', port=8000, debug=True)
+
+    # Open a window
+    webbrowser.open("http://localhost:{}/".format(port), new=2)
 
 if __name__ == '__main__':
     main()
