d823c55c9ff88cfad8cfcc2ab761c5fa328f5285
gaoce <gaoce@coe.neu.edu>
2015-04-27 16:34:00 -0400

    Update layout

--- timevis/static/js/main.js
+++ timevis/static/js/main.js
@@ -1,6 +1,21 @@
 function viewModel(){
     var self = this;
-    self.activeSec = ko.observable('design');
-    self.activeOpt = ko.observable('exp');
+    self.activeSec = ko.observable('design');  // Active section
+    self.activeOpt = ko.observable('exp');     // Active option
+    self.activeFun = ko.observable('exp_old'); // Active function
+    self.experiments = ko.observableArray(['AAA', 'BBB']);
+    self.exp_vars = ko.observableArray([new ExpVar()]);
+    self.var_types = ['categorical', 'numerical (integer)', 'numerical (flat)'];
+
+	// Add a new variable
+    self.addVar = function() { self.exp_vars.push(new ExpVar()) };
+    // Remove a variable
+    self.removeVar = function(variable) { self.exp_vars.remove(variable) };
 }
+
+var ExpVar = function() {
+    var self = this;
+    self.name = ko.observable();
+    self.type = ko.observable();
+};
 ko.applyBindings(new viewModel());
