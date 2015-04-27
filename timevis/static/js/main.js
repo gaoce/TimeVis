ad50364588068c5ff9cf10480ca02e8ff924c4fb
gaoce <gaoce@coe.neu.edu>
2015-04-27 15:10:14 -0400

    Update main.js

--- timevis/static/js/main.js
+++ timevis/static/js/main.js
@@ -1,11 +1,6 @@
 function viewModel(){
     var self = this;
-    self.active = ko.observable('');
-    self.activeA = ko.observable(false);
-    self.activeB = ko.observable(false);
-    self.activeC = ko.observable(false);
-    self.activeAItem = function(){self.active('a')};
-    self.activeBItem = function(){self.active('b')};
-    self.activeCItem = function(){self.active('c')};
+    self.activeSec = ko.observable('design');
+    self.activeOpt = ko.observable('exp');
 }
 ko.applyBindings(new viewModel());
