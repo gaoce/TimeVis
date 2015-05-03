20bfc21fa519d2f8c9547a805ae263801d8c28e9
gaoce <gaoce@coe.neu.edu>
2015-05-02 23:22:10 -0400

    Start RESTful API

--- timevis/views.py
+++ timevis/views.py
@@ -1,5 +1,8 @@
 from . import app
 from flask import render_template
+from flask.ext import restful
+
+api = restful.Api(app)
 
 
 @app.route('/')
@@ -11,3 +14,24 @@ def index():
 @app.route('/test')
 def test():
     return render_template('control.html')
+
+
+class Experiment(restful.Resource):
+    pass
+
+
+class Layout(restful.Resource):
+    pass
+
+
+class Plate(restful.Resource):
+    pass
+
+
+class TimeSeries(restful.Resource):
+    pass
+
+api.add_resource(Experiment, '/experiment')
+api.add_resource(Layout, '/layout')
+api.add_resource(Plate, '/plate')
+api.add_resource(TimeSeries, '/timeseries/')
