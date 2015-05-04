b0d09c704f9ec3b08f051244bf145d9b4167399d
gaoce <gaoce@coe.neu.edu>
2015-05-03 20:29:22 -0400

    Update views

--- timevis/views.py
+++ timevis/views.py
@@ -6,14 +6,8 @@ api = restful.Api(app)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
-
-
-@app.route('/test')
-def test():
-    return render_template('control.html')
 
 
 class Experiment(restful.Resource):
