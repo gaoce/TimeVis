c34653b58f0f5cf6b06885b1da90a2a87b071eb2
gaoce <gaoce@coe.neu.edu>
2015-05-04 21:14:27 -0400

    Add argument parsing for app

--- timevis/app.py
+++ timevis/app.py
@@ -6,12 +6,23 @@ import webbrowser
 app = Flask('timevis')
 
 
+def parse_args():
+    args = {}
+    args.browser = None
+
+    return args
+
+
 def main(port=8000):
     # Begin the server
     app.run(host='0.0.0.0', port=8000, debug=True)
 
+    # Parse args
+    args = parse_args()
+
     # Open a window
-    webbrowser.open("http://localhost:{}/".format(port), new=2)
+    if args.browser:
+        webbrowser.open("http://localhost:{}/".format(port), new=2)
 
 if __name__ == '__main__':
     main()
