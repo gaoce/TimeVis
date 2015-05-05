a9a2870fe4ab0dbb4cb4a31c3ad4f767c01843ef
gaoce <gaoce@coe.neu.edu>
2015-05-05 10:12:30 -0400

    Improve models

--- timevis/models.py
+++ timevis/models.py
@@ -1,39 +1,53 @@
+import os.path
 from sqlalchemy import ForeignKey, Column, Integer, String, Float, Time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-import os.path
 
+# Create Base, Session and engine
 Base = declarative_base()
-data_path = os.path.join(os.path.dirname(__file__), 'db')
-db_path = os.path.join(data_path, 'timevis.db')
+db_path = os.path.join(os.path.dirname(__file__), 'db', 'timevis.db')
 engine = create_engine('sqlite:///{}'.format(db_path))
-
-# Create Session class and instance to make query
 Session = sessionmaker(bind=engine)
 
 
 class Experiment(Base):
-    """ Experiment table contains information describing experiments
+    """Experiment table contains information describing experiments
     """
 
     __tablename__ = 'experiments'
 
     id = Column(Integer, primary_key=True)
+
+    # Experiment name, must be unique
     name = Column(String, nullable=False, unique=True)
+
+    # Names of users, comma separated
     user = Column(String)
+
+    # Well number, either 96 or 384
     well = Column(Integer, nullable=False)
+
+    # Date of measurement
+    # data = Column(Date)
 
     def __repr__(self):
         return "<Experiment({}, {})>".format(self.name, self.well)
 
 
 class Factor(Base):
+    """Factor table contains information describing factors (independent
+    variables)
+    """
     __tablename__ = 'factors'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+
+    # Name of factor, usually a controlled condition, like dose
+    name = Column(String(50), nullable=False)
+
+    # Type of factor, either 'Category', 'Integer', or 'Decimal'
     type = Column(String, nullable=False)
 
     # Foreign key
@@ -47,7 +61,28 @@ class Factor(Base):
         return "<Factor({})>".format(self.name)
 
 
+class Channel(Base):
+    """Channel tables contains information describing measurement
+    """
+    __tablename__ = 'channels'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    # Foreign key
+    id_Experiment = Column(Integer, ForeignKey('experiments.id'))
+
+    # Relationship
+    channels = relationship("Experiment",
+                            backref=backref('channels', order_by=id))
+
+    def __repr__(self):
+        return "<Channel({})>".format(self.name)
+
+
 class Layout(Base):
+    """Layout table describing unique plate setup
+    """
     __tablename__ = 'layouts'
 
     id = Column(Integer, primary_key=True)
@@ -64,28 +99,17 @@ class Layout(Base):
         return "<Layout({})>".format(self.name)
 
 
-class Channel(Base):
-    __tablename__ = 'channels'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-
-    id_Experiment = Column(Integer, ForeignKey('experiments.id'))
-
-    channels = relationship("Experiment", backref=backref('channels',
-                                                          order_by=id))
-
-    def __repr__(self):
-        return "<Channel({})>".format(self.name)
-
-
 class Plate(Base):
+    """Plate table describe invidual plate
+    """
     __tablename__ = 'plates'
 
     id = Column(Integer, primary_key=True)
 
+    # Foreign key
     id_Layout = Column(Integer, ForeignKey('layouts.id'))
 
+    # Relationship
     plates = relationship("Layout", backref=backref('plates', order_by=id))
 
     def __repr__(self):
@@ -98,20 +122,25 @@ class Level(Base):
     __tablename__ = 'levels'
 
     id = Column(Integer, primary_key=True)
-    levels = Column(String)
 
+    # Name of the well
+    well = Column(String, nullable=False)
+
+    # Level of the specific factor, which is indicated by id_Factor
+    level = Column(String)
+
+    # Foreign keys
     id_Layout = Column(Integer, ForeignKey('layouts.id'))
     id_Plate = Column(Integer, ForeignKey('plates.id'))
     id_Factor = Column(Integer, ForeignKey('factors.id'))
 
-    layout_levels = relationship("Layout",
-                                 backref=backref('levels', order_by=id))
-
-    plate_levels = relationship("Plate",
-                                backref=backref('levels', order_by=id))
-
-    factor_levels = relationship("Factor",
-                                 backref=backref('levels', order_by=id))
+    # Relationships
+    # layout_levels = relationship("Layout",
+    #                              backref=backref('levels', order_by=id))
+    # plate_levels = relationship("Plate",
+    #                             backref=backref('levels', order_by=id))
+    # factor_levels = relationship("Factor",
+    #                              backref=backref('levels', order_by=id))
 
 
 class Value(Base):
@@ -120,15 +149,21 @@ class Value(Base):
     __tablename__ = 'values'
 
     id = Column(Integer, primary_key=True)
+
+    # Well name
     well = Column(String, nullable=False)
+
+    # Time of measurement
     time = Column(Time, nullable=False)
+
+    # Value of measurement
     value = Column(Float, nullable=False)
 
     id_Plate = Column(Integer, ForeignKey('plates.id'))
     id_Channel = Column(Integer, ForeignKey('channels.id'))
 
     # Relationships
-    plate_values = relationship("Plate", backref=backref('values', order_by=id))
-
-    channel_values = relationship("Channel",
-                                  backref=backref('values', order_by=id))
+    # plate_values = relationship("Plate",
+    #                             backref=backref('values', order_by=id))
+    # channel_values = relationship("Channel",
+    #                               backref=backref('values', order_by=id))
