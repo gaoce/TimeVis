a765ec01a6cc00ebb3ccbcf99c64c47b1067f526
gaoce <gaoce@coe.neu.edu>
2015-05-05 15:08:04 -0400

    Add database initialize process in local running script

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 import timevis
+import timevis.models as m
 
+
+def init_db():
+    m.Base.metadata.create_all(m.engine)
+
+init_db()
 timevis.app.run(host='0.0.0.0', port=8000)
