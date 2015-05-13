6a524e4598a39f9e7b359e504b6cb345f51fd629
gaoce <gaoce@coe.neu.edu>
2015-05-12 23:57:53 -0400

    Implement Plugin properties

--- timevis/plugin.py
+++ timevis/plugin.py
@@ -1,3 +1,7 @@
+from timevis.models import session
+import pandas as pd
+from timevis.models import (Experiment, Layout, Factor, Level, Plate, Channel,
+                            Value)
 plugins = {}
 
 
@@ -18,12 +22,50 @@ class Plugin(object):
 
     @property
     def experiment(self):
-        pass
+        """
+        Returns
+        -------
+        ret: pandas.DataFrame
+            A data frame with columns: id, name, user, well
+        """
+        query = session.query(Experiment)
+        ret = pd.read_sql(query.statement, query.session.bind)
+        return ret
 
     @property
     def layout(self):
-        pass
+        """
+        Returns
+        -------
+        ret: pandas.DataFrame
+            A data frame with columns: eid, lid, fid, pid, layout_name,
+            factor_name, well, level
+        """
+        query = session.query(Factor.id_experiment.label('eid'),
+                              Layout.id.label('lid'),
+                              Factor.id.label('fid'),
+                              Plate.id.label('pid'),
+                              Layout.name.label('layout_name'),
+                              Factor.name.label('factor_name'),
+                              Level.well, Level.level).\
+            join(Level, Level.id_factor == Factor.id).\
+            join(Layout, Layout.id == Level.id_layout).\
+            join(Plate, Plate.id_layout == Layout.id)
+        ret = pd.read_sql(query.statement, query.session.bind)
+        return ret
 
     @property
     def plate(self):
-        pass
+        """
+        Returns
+        -------
+        ret: pandas.DataFrame
+            A data frame with columns: pid, cid, channel_name, well, time, value
+        """
+        query = session.query(Plate.id.label('pid'), Channel.id.label('cid'),
+                              Channel.name.label('channel_name'), Value.well,
+                              Value.time, Value.value).\
+            join(Channel, Channel.id == Value.id_channel).\
+            join(Value, Value.id_plate == Plate.id)
+        ret = pd.read_sql(query.statement, query.session.bind)
+        return ret
