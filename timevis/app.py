8f518bb02fb47a50d3202c59547064db210d26ed
gaoce <gaoce@coe.neu.edu>
2015-05-19 23:14:58 -0400

    Enable browser option

--- timevis/app.py
+++ timevis/app.py
@@ -10,18 +10,19 @@ app = Flask('timevis')
 def parse_args():
     args = {}
     parser = argparse.ArgumentParser(description='TimeVis')
-    parser.add_argument('-b', '--browser', help='Enable browser')
+    parser.add_argument('-b', '--browser', action='store_true',
+                        help='Enable browser')
     args = parser.parse_args()
 
     return args
 
 
 def main(port=8000):
-    # Begin the server
-    app.run(host='0.0.0.0', port=8000, debug=True)
-
     # Parse args
     args = parse_args()
+
+    # Begin the server
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
     # Open a window
     if args.browser:
