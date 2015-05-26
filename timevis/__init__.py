f81e74896445a0455c368ba6326773e3fceb4760
gaoce <gaoce@coe.neu.edu>
2015-05-26 11:17:04 -0400

    Delete unnecessary texts

--- timevis/__init__.py
+++ timevis/__init__.py
@@ -3,11 +3,3 @@ import views
 import api
 import models
 from plugin import Plugin
-
-# import imp
-#
-# filenames = ['test']
-# pathnames = 'path'
-# for file in filenames:
-#     f, p, d = imp.find_module(file, [pathnames])
-#     imp.load_module(file, f, p, d)
